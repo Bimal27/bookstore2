727e74fc3582aa9071e56a81c4dc4315de4487c7
Bimal27 <bimalsah1996@gmail.com>
2021-08-25 17:36:08 +0200

    changed singlebook to class component

--- src/components copy/SingleBook.jsx
+++ src/components copy/SingleBook.jsx
@@ -1,19 +1,27 @@
 
+import { Component } from 'react'
 import {  Card, Container ,Row} from 'react-bootstrap'
 
 
-const SingleBook = (props) => 
-(
-<Container>
-    <Row className="justify-content-center"> 
-        <Card style={{width:'18rem'}}>
-        <Card.Img variant="top" src={props.book.img} />
-        <Card.Body>
-            <Card.Title>{props.book.title}</Card.Title>
-        </Card.Body>
-        </Card>
-</Row>
-</Container>
-)
+class SingleBook extends Component{
+    state={
+        selected: false
+    }
+    render(){
+        return(
+        <Container>
+            <Row className="justify-content-center"> 
+                <Card style={{width:'18rem'}} onClick={() =>this.setState({selected: !this.state.selected})}
+                style={{border: this.state.selected ? "4px solid green" : "none"}} >
+                <Card.Img variant="top" src={this.props.book.img} />
+                <Card.Body>
+                    <Card.Title>{this.props.book.title}</Card.Title>
+                </Card.Body>
+                </Card>
+        </Row>
+        </Container>
+        )
+    }
+} 
 
 export default SingleBook
